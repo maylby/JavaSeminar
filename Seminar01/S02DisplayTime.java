5774b00abaaba2a6e9548cfbd497de97624a14d5
Vit <mayllby@gmail.com>
2024-02-14 18:48:01 +0300

    Добавил правки

--- Seminar01/S02DisplayTime.java
+++ Seminar01/S02DisplayTime.java
@@ -34,7 +34,5 @@ public class S02DisplayTime {
 		 * LocalTime // работает, только со временем
          */                                        
         System.out.println(LocalDateTime.now());
-        
-    }
-      
+    }  
 }
