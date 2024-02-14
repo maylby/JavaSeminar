9cd743a45f0edc7b153370bbf10cc7074a49049f
Vit <mayllby@gmail.com>
2024-02-14 15:26:35 +0300

    Добавил 'Seminar01'

---
+++ Seminar01/S01PointEntry.java
@@ -0,0 +1,10 @@
+/*
+Java: знакомство и как пользоваться базовым API (семинары)
+Урок 1. Знакомство с языком программирования Java
+https://gb.ru/lessons/414471
+ */
+package JavaSeminar.Seminar01;
+
+public class S01PointEntry {
+    
+}
