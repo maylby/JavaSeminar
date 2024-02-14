632090d1aba6abbd09ecb58101d601f1ec0ea904
Vit <mayllby@gmail.com>
2024-02-14 18:32:01 +0300

    Добавил код

--- Seminar01/S02DisplayTime.java
+++ Seminar01/S02DisplayTime.java
@@ -4,6 +4,7 @@ https://gb.ru/lessons/414471
 
 
 00:31:50
+Вызов метода Даты и Времени
 
 Задание №0. 
 
@@ -15,6 +16,27 @@ https://gb.ru/lessons/414471
  */
 package JavaSeminar.Seminar01;
 
+import java.time.LocalDateTime;
+
 public class S02DisplayTime {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");  // вызывать строку набором "System" или "print",
+                                                // "print" удобнее, подсказка из двух строк
+                                                // ("System.out.println" и "System.err.println") 
+        /*
+        Старые методы:
+         * Data // не рекомендован к использованию
+		        // сохраняется Java для совместимости со старыми версиями
+         * Calendar // тоже старый метод
+
+        Используются в новых версиях Java:
+		 * LocalDate // работает, только с датой
+		 * LocalDateTime // работает с датой и временем
+		 * LocalTime // работает, только со временем
+                     // 
+         */                                        
+        System.out.println(LocalDateTime.now());
+        
+    }
+      
 }
