cb51a45e554c6c64a2eda6bde07ae81d9cfe9e2d
Vit <mayllby@gmail.com>
2024-02-15 22:39:39 +0300

    Добавил комментарий

--- Seminar01/S06CountNumArray2.java
+++ Seminar01/S06CountNumArray2.java
@@ -15,13 +15,18 @@ package JavaSeminar.Seminar01;
 public class S06CountNumArray2 {
 
  	public static void main(String[] args) {
-		int[] array = {1,1,0,1,1,1};
-		int max = getMaxCountOnes(array);
-		System.out.print(max);
+		int[] array = {1,1,0,1,1,1};        // Адаптация кода для C#: 
+                                            // "int[] array" - над методом.
+		int max = getMaxCountOnes(array);   // "int max =" разместить внизу,
+                                            // под "return max", за скобкой.
+        System.out.print(max);              // "print(max)" преобразовать 
+                                            // в "Console.Write(max)"
+                                            // поместить под "int max ="
+                                            // прочее - без изменений.
 	}
     /*
-     * Код задачи из файла "S06CountNumArray",
-     * переобразованный в отдельный метод "getMaxCountOnes",
+     * Код задачи из файла "S06CountNumArray"
+     * переобразован в отдельный метод "getMaxCountOnes"
      * с помощью рефакторинга (функционал VSCode)
      */
     private static int getMaxCountOnes(int[] array) {
@@ -30,8 +35,8 @@ public class S06CountNumArray2 {
 		for (int el: array) {
 			if (el == 1) {
 				count++;
-				// if (count > max) max = count; // v1
-				// max = Math.max(count, max);   // v2
+				// if (count > max) max = count; // v1 (стандартный)
+				// max = Math.max(count, max);   // v2 (вариант VSCode)
 				max = count > max? count : max;  // v3 (теренарный оператор)
 
 			} else {
