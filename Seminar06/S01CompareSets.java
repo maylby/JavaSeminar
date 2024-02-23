6ae9511bc83c318cdde43fea53f73cfa0037ef94
Vit <mayllby@gmail.com>
2024-02-23 17:15:26 +0300

    Добавил решение задачи

--- Seminar06/S01CompareSets.java
+++ Seminar06/S01CompareSets.java
@@ -9,13 +9,41 @@ HashSet (не сохраняет порядок добавления)
 LinkedHashSet (сохраняет порядок добавления)
 TreeSet (сортирует
 
-Задание №0
-1. Создайте HashSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
-Распечатайте содержимое данного множества.
-2. Создайте LinkedHashSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5,
-6, 3}. Распечатайте содержимое данного множества.
-3. Создайте TreeSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
-Распечатайте содержимое данного множества.
 
+Задание №0
+(преподаватель)
 
  */
+package JavaSeminar.Seminar06;
+
+import java.util.Set;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+/**
+ * S01CompareSets
+ * 
+ * 1. Создайте HashSet, заполните его следующими числами:
+ * {3, 6, 43, 8, 11, 7, 33, 3, 4, 8}.
+ * Распечатайте содержимое данного множества.
+ * 2. Создайте LinkedHashSet, заполните его следующими числами:
+ * {3, 6, 43, 8, 11, 7, 33, 3, 4, 8}.
+ * Распечатайте содержимое данного множества.
+ * 3. Создайте TreeSet, заполните его следующими числами:
+ * {3, 6, 43, 8, 11, 7, 33, 3, 4, 8}.
+ * Распечатайте содержимое данного множества.
+ */
+public class S01CompareSets {
+    public static void main(String[] args) {
+        Set<Integer> nashSet = new HashSet<>(Arrays.asList(3, 6, 43, 8, 11, 7, 33, 3, 4, 8));
+
+        Set<Integer> linkedHashSet = new LinkedHashSet<>(Arrays.asList(3, 6, 43, 8, 11, 7, 33, 3, 4, 8));
+        Set<Integer> treeSet = new TreeSet<>(Arrays.asList(3, 6, 43, 8, 11, 7, 33, 3, 4, 8));
+        System.out.println(nashSet);
+        System.out.println(linkedHashSet);
+        System.out.println(treeSet);    
+    }
+
+}
