551c1784d24f07061c8f31e39e294dcf7eda3e98
Vit <mayllby@gmail.com>
2024-03-04 01:45:33 +0300

    Добавил решение

--- Seminar05/Task02/IsomorfWords.java
+++ Seminar05/Task02/IsomorfWords.java
@@ -31,6 +31,63 @@ Output: true
 */
 package JavaSeminar.Seminar05.Task02;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsomorfWords {
-    
+
+    public static void main(String[] args) {
+    String s = "paper", t = "title";
+    boolean res = isIsomorphic(s, t);
+    System.out.println(res);		
 }
+/*
+* метод "isIsomorphic" (?) сравнивает строки "strOne", "strTwo"
+*/
+public static boolean isIsomorphic(String strOne, String strTwo) {
+    if (strOne.length() != strTwo.length()) { // если длина слов разная
+        return false;
+    }
+    if (strOne.equals(strTwo)) { // если слова одинаковые
+        return true;
+    }
+    Map<Character, Character> dict = new HashMap<>();
+    for (int i = 0; i < strOne.length(); i++) {
+        char one = strOne.charAt(i);
+        char two = strTwo.charAt(i);
+        /**
+        * если в "ключе" (one) есть совпадающий символ (см. следующее условие)
+        */
+        if (dict.containsKey(one)) { 
+            /**
+            * Если "ключ" (one) не совпадает со "значением" (two),
+            * возвращаем "false" (ложь), слова не изоморфны
+            */
+            if (dict.get(one) != two) {/* если 1-й и 3-й "ключи" совпали,
+                            * но 1-е и 3-е "значения" разные,
+                            * например, как в "paper" и "tiles", 
+                            * то слова не изоморфны (false) */
+                return false;
+            }
+        /** 
+        * "Обратная" проверка:
+        * "containsValue" проверяет значения, т.е. сами символы
+        * Если символ (two) повторился, но в ключе (one) повтора нет, 
+        * вернуть "false" (ложь), т.к. слова не изоморфны
+        */
+        } else if (dict.containsValue(two)) {  /* 
+                                                * Без проверки значений,
+                                                * слова "power" и "title"
+                                                * программа сочтёт изоморфными, 
+                                                * т.к. в коде нет указаний на то, 
+                                                * что значения не могут совпадать, 
+                                                * если не совпали ключи 
+                                                */
+            return false; 
+        } else {
+            dict.put(one, two); // иначе, добавляем найденную пару
+        }
+    }
+    return true; // возвращаем истину (слова изоморфны)
+}}
+
