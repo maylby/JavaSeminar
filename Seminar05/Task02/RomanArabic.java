b0932f66bd205c34db43fa535cb4dae5649864a5
Vit <mayllby@gmail.com>
2024-03-04 04:05:15 +0300

    Добавил правки

--- Seminar05/Task02/RomanArabic.java
+++ Seminar05/Task02/RomanArabic.java
@@ -24,41 +24,40 @@ package JavaSeminar.Seminar05.Task02;
 
 import java.util.HashMap;
 import java.util.Map;
-/*
-Написать метод, который переведет число 
-из римского формата записи в арабский.
-Например, MMXXII = 2022
- */
+
 public class RomanArabic {
-            public static void main(String[] args) {
-		String romanNum = "MMXXII";
-		System.out.println(romanToArabic(romanNum)); // передаём в метод исходную строку
-	}
-	
-	public static Map<Character, Integer> getRomanArabicMap() {
-		Map<Character, Integer> map = new HashMap<>();
-		map.put('I', 1);
-		map.put('V', 5);
-		map.put('X', 10);
-		map.put('L', 50);
-		map.put('C', 100);
-		map.put('L', 500);
-		map.put('M', 1000);
-		return map;
-	}
-	public static int romanToArabic(String romanNumber) {
-		Map<Character, Integer> map = new getRomanArabicMap<>();
-		int result = 0;
-		int prefValue = 0;
-		for (int i = romanNumber.length - 1; i >= 0; i--) {
-			int currentValue = map.get(romanNumber.charAt(i));
-			if (prefValue < prefValue) {
-				result -= currentValue;
-			} else {
-				result += currentValue;
-			}
-			prefValue = currentValue;
-		}
-		return result;
-	}
+
+public static void main(String[] args) {
+    String romanNum = "MMXXIV";
+    System.out.println(romanToArabic(romanNum));
+
+    }
+    public static Map<Character, Integer> getRomanArabicMap() {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        return map;
+
+    }
+    public static int romanToArabic(String romanNumber) {
+        Map<Character, Integer> map = getRomanArabicMap();
+        int result = 0;
+        int prefValue = 0;
+        for (int i = romanNumber.length()-1; i >= 0; i--) {
+            int currentValue = map.get(romanNumber.charAt(i));
+            if (currentValue < prefValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+            prefValue = currentValue;
+        }
+        return result;
+
+    }
 }
