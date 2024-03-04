6060b966980cf474761e9166a4ec0d44ac9b55a3
Vit <mayllby@gmail.com>
2024-03-04 12:48:36 +0300

    Добавил перечень

---
+++ TestWork/ProductData.java
@@ -0,0 +1,55 @@
+package JavaSeminar.TestWork;
+
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class ProductData {
+    static Set<Laptops> createLaptopsSet() {
+
+        Map<String, Integer> rams = new LinkedHashMap<>();
+        rams.put("8GB", 4000);
+        rams.put("16GB", 8000);
+        rams.put("32GB", 16000);
+        rams.put("64GB", 32000);
+        rams.put("128GB", 64000);
+
+        Map<String, Integer> hdds = new LinkedHashMap<>();
+        hdds.put("500GB", 4000);
+        hdds.put("1TB", 9000);
+        hdds.put("2TB", 14000);
+
+        Map<String, Integer> cpus = new TreeMap<>();
+        cpus.put("Intel Core i3", 7000);
+        cpus.put("AMD Ryzen 5", 8000);
+        cpus.put("Intel Core i5", 9000);
+        cpus.put("AMD Ryzen 7", 11000);
+        cpus.put("Intel Core i7", 14000);
+        cpus.put("AMD Ryzen 9", 23000);
+        cpus.put("Intel Core i9", 28000);
+        cpus.put("AMD Ryzen 3", 6000);
+        cpus.put("Intel Core m3", 5000);
+        // cpus.put("Qualcomm Snapdragon 8cx", 11000);
+
+        // Map<String, Integer> graphicsProcessors = new TreeMap<>();
+        // graphicsProcessors.put("NVIDIA GeForce GTX 1660Ti 6GB", 23000);
+        // graphicsProcessors.put("AMD Radeon RX 5700XT 8GB", 28000);
+        // graphicsProcessors.put("NVIDIA GeForce RTX 2070SUPER 8GB", 33000);
+        // graphicsProcessors.put("AMD Radeon RX 5600XT 6GB", 18000);
+        // graphicsProcessors.put("NVIDIA GeForce RTX 2080Ti 11GB", 55000);
+
+        Map<String, Integer> screenSizes = new LinkedHashMap<>();
+        screenSizes.put("13.0\"", 25000);
+        screenSizes.put("14.0\"", 30000);
+        screenSizes.put("15.0\"", 35000);
+        screenSizes.put("17.0\"", 40000);
+        screenSizes.put("12.0\"", 20000);
+
+
+    }
+}
+
+
